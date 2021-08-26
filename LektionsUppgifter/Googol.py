77212325e1e35b6e75482908e1360728c2c35ed3
Rasmus Arwidi <rasmus.arwidi@skola.taby.se>
2021-08-26 08:54:04 +0200

    2021-08-26

---
+++ LektionsUppgifter/Googol.py
@@ -0,0 +1,12 @@
+import time
+import datetime
+
+#currentTime = time.time()
+#googol = 10**100
+#seconds = googol * 60
+#currentTime + seconds
+#currentDate = time.gmtime(currentTime + seconds)
+#print(currentDate)
+
+now = datetime.datetime.now()
+print(now.hour)
---
+++ README.md
@@ -0,0 +1,1 @@
+"# RasmusA-Pr2" 
