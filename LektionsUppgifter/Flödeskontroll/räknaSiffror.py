948c2acdd2d61fee08e2ef9487dddb094e217beb
Rasmus Arwidi <rasmus.arwidi@skola.taby.se>
2021-09-06 13:49:42 +0200

    2021-09-006

--- LektionsUppgifter/Flödeskontroll/räknaSiffror.py
+++ LektionsUppgifter/Flödeskontroll/räknaSiffror.py
@@ -1,6 +1,6 @@
 string = input("sträng: ")
 numCount = 0
-num = ["1","2","3","4","5","6","7","8","9"]
+num = ["0","1","2","3","4","5","6","7","8","9"]
 for i in string:
     if i in num: 
         numCount += 1
