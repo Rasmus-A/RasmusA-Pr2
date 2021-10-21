cfd45afea4a259704f64fde72652a85ab58c3d77
Rasmus Arwidi <rasmus.arwidi@skola.taby.se>
2021-10-21 09:40:58 +0200

    2021-10-21

--- Progolymp2021/ProgolympKval.py
+++ Progolymp2021/ProgolympKval.py
@@ -1,38 +1,29 @@
-R = [[1, 1, 1, 1],
-     [1, 0, 0, 1], 
-     [1, 1, 1, 1], 
-     [1, 1, 1, 1]]
+top = []
+right = []
+left = []
+bottom = []
+temp = []
+score = 0
 
-G = [[1, 1, 1, 1],
-     [1, 1, 1, 1], 
-     [1, 1, 1, 1], 
-     [1, 1, 1, 1]]
 
-B = [[1, 1, 1, 1],
-     [0, 1, 1, 0], 
-     [0, 1, 1, 0], 
-     [1, 1, 1, 1]]
+print("Give input as r, g, or b separated by a space x4. Going top down or left to right ")
+top = input("Top row: ").split()
+right = input("Right row: ").split()
+left = input("Left row: ").split()
+bottom = input("Bottom row: ").split()
 
-checkp1 = [[0, 0, 0, 0],
-           [0, 0, 0, 0], 
-           [0, 0, 0, 0], 
-           [0, 0, 0, 0]]
+for i in range(4):
+     for j in range(4):
+          full = ["r", "g", "b"]
+          temp.append(top[i])
+          temp.append(bottom[i])
+          temp.append(right[j])
+          temp.append(left[j])
+          pairs = ([(value, "True") for value in temp])
+          for a, b in pairs:
+               if a == True:
+                    full.remove(b)
+          print(full)
+          temp.clear()
 
-result = [[0, 0, 0, 0],
-          [0, 0, 0, 0], 
-          [0, 0, 0, 0], 
-          [0, 0, 0, 0]]
-
-# iterate through rows
-for i in range(len(R)):
-   # iterate through columns
-   for j in range(len(R[0])):
-       checkp1[i][j] = R[i][j] + G[i][j]
-
-for i in range(len(checkp1)):
-   # iterate through columns
-   for j in range(len(checkp1[0])):
-       result[i][j] = checkp1[i][j] + B[i][j]
-
-for r in result:
-   print(r)
+#print(score)
