cfa79e74ada8e238c727d2e44e7a3e6c6b1a8f77
Rasmus Arwidi <rasmus.arwidi@skola.taby.se>
2021-10-25 14:13:00 +0200

    2021-10-25

--- Progolymp2021/ProgolympKval.py
+++ Progolymp2021/ProgolympKval.py
@@ -2,28 +2,19 @@ top = []
 right = []
 left = []
 bottom = []
-temp = []
+temp = set()
 score = 0
-
-
-print("Give input as r, g, or b separated by a space x4. Going top down or left to right ")
+print("Give input as r, g, or b separated by a space. Going top down or left to right ")
 top = input("Top row: ").split()
 right = input("Right row: ").split()
 left = input("Left row: ").split()
 bottom = input("Bottom row: ").split()
-
-for i in range(4):
-     for j in range(4):
-          full = ["r", "g", "b"]
-          temp.append(top[i])
-          temp.append(bottom[i])
-          temp.append(right[j])
-          temp.append(left[j])
-          pairs = ([(value, "True") for value in temp])
-          for a, b in pairs:
-               if a == True:
-                    full.remove(b)
-          print(full)
-          temp.clear()
-
-#print(score)
+for i in range(len(top)):
+     for j in range(len(right)):
+          temp.add(top[i])
+          temp.add(bottom[i])
+          temp.add(right[j])
+          temp.add(left[j])
+          if len(temp) == 3:
+               score += 1
+print(score)
